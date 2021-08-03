974aac9afb1d79f9239a7bb7d66313cfaa195124
N0FreeLunch <consilience90@gmail.com>
2021-08-03 22:54:14 +0900

    btn added

--- week4/assignment/ghibli_code.js
+++ week4/assignment/ghibli_code.js
@@ -1,6 +1,18 @@
 console.log("지브리 코드 : ", data);
 
+// 가상 돔을 만든다.
 var contents = document.createElement('content');
+var prevBtn = document.createElement('button');
+var nextBtn = document.createElement('button');
+
+// 버튼 이름을 추가한다.
+prevBtn.innerText = "prev page";
+nextBtn.innerText = "next page";
+
+// body 태그에 달아 준다. 순서대로
+// 만든 순서에 상관 없이 넣는 순서에 따라 넣어진다.
+document.body.appendChild(prevBtn);
+document.body.appendChild(nextBtn);
 document.body.appendChild(contents);
 
 // {} 안에 key : vlaue 형식으로 써 넣을 것을 literal object라고 한다.
@@ -56,10 +68,27 @@ console.log("long : ", movieContent(data[0]));
 // contents.innerHTML = movieContent(data[0]) + movieContent(data[1]) + movieContent(data[2]) + movieContent(data[3]) + movieContent(data[4]);
 
 console.log("data length : ", data.length);
-var sumMovieContents = "";
-for (var i = 0; i < data.length; i++) {
-  sumMovieContents = sumMovieContents + movieContent(data[i]);
-}
-console.log(sumMovieContents);
+// var sumMovieContents = "";
+// for (var i = 0; i < data.length; i++) {
+//   sumMovieContents = sumMovieContents + movieContent(data[i]);
+// }
+// console.log(sumMovieContents);
+//
+// contents.innerHTML = sumMovieContents;
 
-contents.innerHTML = sumMovieContents;
+
+var onePage = function (startIndex, endIndex) {
+  var sumMovieContents = "";
+  for (var i = startIndex; i < endIndex; i++) {
+    sumMovieContents = sumMovieContents + movieContent(data[i]);
+  }
+  return sumMovieContents;
+}
+
+contents.innerHTML = onePage(0,4);
+
+var btnClick = function () {
+  contents.innerHTML = onePage(5,9);
+}
+
+nextBtn.onclick = btnClick;
